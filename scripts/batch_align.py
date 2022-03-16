5f236353440998d5d503cdda543abb04556e7483
Karel Břinda <karel.brinda@gmail.com>
2022-03-16 10:57:14 -0400

    Cleanup

--- scripts/batch_align.py
+++ scripts/batch_align.py
@@ -13,58 +13,6 @@ from pathlib import Path
 from xopen import xopen
 from pprint import pprint
 
-KEEP = 100
-"""
-For every read we want to know top 100 matches
-
-Approach:
-    - for every read keep a buffer in memory
-    - iterate over all translated cobs outputs
-    - keep just top k scores
-"""
-
-
-def cobs_iterator(cobs_matches_fn):
-    """Iterator for cobs matches.
-
-    Assumes that cobs ref names start with a random sorting prefix followed by
-    an underscore (embedded by Leandro).
-
-    Args:
-        cobs_matches_fn (str): File name of cobs output.
-
-    Returns:
-        (qname, matches): Qname and list of assignments of the same query, in the form (ref, kmers)
-
-    Todo:
-        - if necessary in the future, add batch name from the file name
-    """
-    qname = None
-    matches_buffer = []
-    batch = os.path.basename(cobs_matches_fn).split("____")[0]
-    print(f"Translating matches {cobs_matches_fn}", file=sys.stderr)
-    with xopen(cobs_matches_fn) as f:
-        for x in f:
-            x = x.strip()
-            if not x:
-                continue
-            if x[0] == "*":
-                ## HEADER
-                # empty buffer
-                if qname is not None:
-                    yield qname, batch, matches_buffer
-                    matches_buffer = []
-                # parse header
-                parts = x[1:].split("\t")
-                qname = parts[0].split(" ")[0]  # remove fasta comments
-                nmatches = int(parts[1])
-            else:
-                ## MATCH
-                tmp_name, kmers = x.split()
-                rid, ref = tmp_name.split("_")
-                matches_buffer.append((ref, kmers))
-    yield qname, batch, matches_buffer
-
 
 def readfq(fp):  # this is a generator function
     # From https://github.com/lh3/readfq/blob/master/readfq.py
@@ -99,8 +47,10 @@ def readfq(fp):  # this is a generator function
                 yield name, seq, None  # yield a fasta record instead
                 break
 
+
 def iterate_over_batch(asms_fn):
     pass
+
 
 def load_qdicts(query_fn):
     with xopen(query_fn) as fo:
@@ -112,6 +62,7 @@ def load_qdicts(query_fn):
 def minimap2(rfa, qfa):
     pass
 
+
 def map_queries_to_batch(asms_fn, query_fn):
     qname_to_qfa, rname_to_qname = load_qdicts(query_fn)
     return
@@ -119,8 +70,9 @@ def map_queries_to_batch(asms_fn, query_fn):
     for rname, rfa in iterate_over_batch(asms_fn):
         for qname in rname_to_qname[rname]:
             qfa = rname_to_qfa[qname]
-            result=minimap2(rfa, qfa)
+            result = minimap2(rfa, qfa)
             print(result)
+
 
 def main():
 
@@ -139,7 +91,7 @@ def main():
     )
 
     args = parser.parse_args()
-    map_queries_to_batch(args.batch_fn,args.query_fn)
+    map_queries_to_batch(args.batch_fn, args.query_fn)
 
 
 if __name__ == "__main__":
