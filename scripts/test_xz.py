979e975a2d96265b7871423c71141c8fbc10ff68
Karel Břinda <karel.brinda@gmail.com>
2022-03-24 10:10:21 -0400

    Add script to test xz files

---
+++ scripts/test_xz.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+import argparse
+import collections
+import lzma
+import os
+import re
+import sys
+
+
+def error(*msg):
+    print(*msg, file=sys.stderr)
+
+
+def test_xz(fn):
+    code = 0
+
+    s = os.path.getsize(fn)
+    if s < 100000:
+        error(f"File {fn} is too small, likely corrupted")
+        code = 1
+
+    try:
+        with lzma.open(fn) as f:
+            a = f.read(10)
+    except lzma.LZMAError:
+        error(f"File {fn} is not a valid xz archive")
+        code = 2
+
+    sys.exit(code)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Test an xz file")
+
+    parser.add_argument(
+        'xz',
+        metavar='file.xz',
+        help='File to test',
+    )
+
+    args = parser.parse_args()
+
+    test_xz(args.xz)
+
+
+if __name__ == "__main__":
+    main()
