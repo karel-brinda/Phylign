4119663b0e37f15919abe11eaabf7a57c173d939
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-16 22:59:24 +0100

    Updating install_cobs.sh

--- scripts/install_cobs.sh
+++ scripts/install_cobs.sh
@@ -3,7 +3,16 @@ mkdir -p cobs_installation && cd cobs_installation
 curl -L https://www.dropbox.com/s/o1wnfebfs33oclq/cobs-0.2.0.tar.gz?dl=1 -o cobs.tar.gz
 tar -zxvf cobs.tar.gz
 mkdir build && cd build
-cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCONDA=1 ..
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCONDA=1 ..
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  cmake -DCMAKE_C_COMPILER=x86_64-apple-darwin*-gcc-11 -DCMAKE_CXX_COMPILER=x86_64-apple-darwin*-g++-11 -DCMAKE_BUILD_TYPE=Release ..
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
 make -j1
 make test
 cd ../../
