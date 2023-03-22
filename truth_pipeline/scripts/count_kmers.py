9e42ab1289b1bcfd77b4c37f883ccada09fd7eaa
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-03-22 22:26:30 +0000

    Fixing small issue with kmer counting script

--- truth_pipeline/scripts/count_kmers.py
+++ truth_pipeline/scripts/count_kmers.py
@@ -9,7 +9,7 @@ def count_kmers(batch_dir: Path, kmer_size: int, output_dir: Path):
             file = file.resolve()
             filename = file.name
             kmer_counts_file = output_dir/f"{filename}.kmer_counting.k_{kmer_size}.fa"
-            subprocess.check_call(f"jellyfish count -m {kmer_size} -s 100M -t 1 <(zcat {file}) && jellyfish dump mer_counts.jf > {kmer_counts_file}")
+            subprocess.check_call(f"jellyfish count -m {kmer_size} -s 100M -t 1 <(zcat {file}) && jellyfish dump mer_counts.jf > {kmer_counts_file}", shell=True)
 
 
 count_kmers(Path(snakemake.input.assembly_batch), int(snakemake.params.k), Path(snakemake.output.kmer_dir))
