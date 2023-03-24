5c34586d54f0772fc283dd15d535922f0f4ba68b
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-03-24 12:55:37 +0000

    Updating truth pipeline

--- truth_pipeline/scripts/count_kmers.py
+++ truth_pipeline/scripts/count_kmers.py
@@ -2,15 +2,18 @@ from pathlib import Path
 import subprocess
 
 
-def count_kmers(batch_dir: Path, kmer_size: int, output_dir: Path):
+def count_kmers(batch_dir: Path, reads_file: Path, kmer_size: int, output_dir: Path):
     output_dir.mkdir(parents=True, exist_ok=True)
     for file in batch_dir.iterdir():
         if file.name.endswith(".fa.gz"):
             file = file.resolve()
             filename = file.name
             kmer_counts_file = output_dir/f"{filename}.kmer_counting.k_{kmer_size}.fa"
-            subprocess.check_call(f"zcat {file} | jellyfish count -m {kmer_size} -s 100M -t 1 /dev/fd/0", shell=True)
+            subprocess.check_call(f"zcat {file} | jellyfish count -m {kmer_size} -s 100M -t 1 --if {reads_file} /dev/fd/0", shell=True)
             subprocess.check_call(f"jellyfish dump mer_counts.jf > {kmer_counts_file}", shell=True)
 
 
-count_kmers(Path(snakemake.input.assembly_batch), int(snakemake.params.k), Path(snakemake.output.kmer_dir))
+count_kmers(Path(snakemake.input.assembly_batch),
+            Path(snakemake.input.reads),
+            int(snakemake.params.k),
+            Path(snakemake.output.kmer_dir))
