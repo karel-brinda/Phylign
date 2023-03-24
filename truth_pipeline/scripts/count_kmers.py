6d4054e429002637b2ea64dc826572933b80681b
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-03-24 18:04:22 +0000

    Fixing count kmers script

--- truth_pipeline/scripts/count_kmers.py
+++ truth_pipeline/scripts/count_kmers.py
@@ -9,7 +9,7 @@ def count_kmers(batch_dir: Path, reads_file: Path, kmer_size: int, output_dir: P
             file = file.resolve()
             filename = file.name
             kmer_counts_file = output_dir/f"{filename}.kmer_counting.k_{kmer_size}.fa"
-            subprocess.check_call(f"zcat {file} | jellyfish count -m {kmer_size} -s 100M -t 1 --if {reads_file} /dev/fd/0", shell=True)
+            subprocess.check_call(f"zcat {file} | jellyfish count --canonical -m {kmer_size} -s 100M -t 1 --if {reads_file} /dev/fd/0", shell=True)
             subprocess.check_call(f"jellyfish dump mer_counts.jf > {kmer_counts_file}", shell=True)
 
 
